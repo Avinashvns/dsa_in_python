c8499e10236be77a830162b42f29152f26098aee
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-26 20:27:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dsa_in_python
+ beginner to advance
---
+++ list/sort.py
@@ -0,0 +1,8 @@
+a = [16, 0, 1, 9, 70]
+print(a)
+
+a.sort()
+print("Sorting assending : ", a)
+
+a.sort(reverse=True)
+print("Sorting decending : ", a)
