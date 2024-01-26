bf5a172f06f8df940386f20d3e0e3eb76b889337
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2024-01-26 20:52:41 +0530

    lisrt sort with function of last character

--- list/sort.py
+++ list/sort.py
@@ -1,13 +1,13 @@
-# Sorting in string with Key (lenth of String)
+# Sorting with Function
 
-a = ["cat", "dog", "ball", "apple", "bee", "a"]
+a = ["cat", "dog", "ball", "apple"]
 
-# Assecending order
-a.sort(key=len)
-print("key ", a)
 
-# Desecending Order
-a.sort(key=len, reverse=True)
+def lastChar(s):
+    print(s)
+    # return s[len(s)-1]
+    return s[-1]
+
+
+a.sort(key=lastChar)
 print(a)
-
-print(len("name"))
